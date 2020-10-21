22b0f270e9f65f06debbabd977d0286d5d9540cb
DBlaNQ <kikusko123456789@gmail.com>
2020-10-21 14:54:37 +0200

    comands

--- main.js
+++ main.js
@@ -7,29 +7,50 @@ client.on('ready', () => {
 })
 
 client.on('message', msg =>{
-    if (msg.content == "-kick"){
-        msg.guild.kick
-    }
-    if(msg.content == "-me"){
-        msg.channel.send("Zdravo! Jas sum Sare Andreevski, Imam 10 godini i najubav fudbalerski tim mi e Arsenal!");
-    }
-    if(msg.content == 'pog' || msg.content == "poggers"){
-        msg.channel.send(" ", {files: ["https://i0.wp.com/nerdschalk.com/wp-content/uploads/2020/08/pogger.png?resize=311%2C307&ssl=1"]})
-    }
-    if(msg.content == 'th' || msg.content == "trihard"){
-        msg.channel.send(" ", {files: ["https://www.streamscheme.com/wp-content/uploads/2020/04/TriHard.png"]})
-    }
-    if(msg.content == 'mac' || msg.content == 'macmiller'){
-        msg.channel.send(" ", {files: ["https://cdn.discordapp.com/attachments/765918107247443993/768136165798314062/avatars-000317096184-q0upfw-t500x500.jpg"]})
-    }
-    if(msg.content == 'petar'){
-        msg.channel.send(" ", {files: ["https://cdn.discordapp.com/attachments/765918107247443993/768136882898337822/122243792_1080335655735319_4810909954261953407_n.jpg"]})
-    }
-    if(msg.content == 'ivo'){
-        msg.channel.send(" ", {files: ["https://cdn.discordapp.com/attachments/765918107247443993/768137958427066452/2019-02-09_21.51.44.jpg"]})
-    }
-    if(msg.content == 'daved' || msg.content == 'dado' || msg.content == 'david'){
-        msg.channel.send(" ", {files: ["https://cdn.discordapp.com/attachments/765918107247443993/768138873192054835/dado.PNG"]})
+    switch (msg.content){
+        //Bruh
+        case '-me':
+            msg.channel.send("Zdravo! Jas sum Sare Andreevski, Imam 10 godini i najubav fudbalerski tim mi e Arsenal!");
+            break;
+        //ADMIN COMMANDS
+        case '-kick':
+            if(msg.member.hasPermission("kick_members") || msg.member.hasPermission("administrator")){
+                msg.mentions[0].kick()
+            }
+            break;
+        case '-rate':
+            let choek = msg.content.split(" ")
+            msg.channel.send(`I give ${choek[1]} a ${Math.floor(Math.random(10) + 1)}/10`)
+            break;
+        //EPIC COMMANDS
+        case 'pog':
+        case "poggers":
+            msg.channel.send(" ", {files: ["img/pogger.png"]})
+            break;
+        case 'th':
+        case "trihard":
+            msg.channel.send(" ", {files: ["https://www.streamscheme.com/wp-content/uploads/2020/04/TriHard.png"]})
+            break;
+        case 'mac':
+        case 'macmiller':
+            msg.channel.send(" ", {files: ["img/mac.jpg"]})
+            break;
+        case 'petar':
+            msg.channel.send(" ", {files: ["img/petar.jpg"]})
+            break;
+        case 'ivo':
+            msg.channel.send(" ", {files: ["img/ivo.jpg"]})
+            break
+        case 'daved':
+        case 'dado':
+        case 'david':
+            msg.channel.send(" ", {files: ["img/dado.png"]})
+            break;
+        case 'kiko':
+        case 'miz':
+        case 'mizh':
+            msg.channel.send(" ", {files: ["img/kiko.png"]})
+            break;
     }
 })
 
