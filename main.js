6590f058013765598800c6fc3cb1fe644b66b171
DBlaNQ <kikusko123456789@gmail.com>
2020-11-25 19:20:54 +0100

    lmao

--- main.js
+++ main.js
@@ -47,16 +47,30 @@ client.on('message', async(message) =>{
             message.channel.send(`I give ${choek[1]} a ${Math.floor(Math.random() * 10)}/10`)
             break;
         //MUSIC COMMANDS
-        case 'play':
-            if(args.length === 0) return message.channel.send("Dobro be glupav si da neznajs da napishish -play <video nekoe tuka>? Weirdchamp")
-            execute(message, serverQueue)
-            break;
-        case 'skip':
-            skip(message, serverQueue)
-            break;
-        case 'leave':
-            stop(message, serverQueue);
-            break;
+          case 'play':
+                execute(message, serverQueue);
+                break;
+            case 'stop':
+                stop(message, serverQueue);
+                break;
+            case 'fskip':
+                skip(message, serverQueue);
+                break;
+            case 'skip':
+                vSkip(serverQueue);
+                break;
+            case 'pause':
+                pause(serverQueue);
+                break;
+            case 'resume':
+                resume(serverQueue);
+                break;
+            case 'loop':
+                Loop(args, serverQueue);
+                break;
+            case 'queue':
+                Queue(serverQueue);
+                break;
 
         //NSFW
         case 'ass':
@@ -97,62 +111,57 @@ client.on('message', async(message) =>{
     }
 
     async function execute(message, serverQueue){
+        if(args.length <= 0)
+            return message.channel.send("Please write the name of the song")
+
         let vc = message.member.voice.channel;
-            if(vc){
-                let result = await searcher.search(args.join(" "), { type: 'video' });
-                const songInfo = await ytdl.getInfo(result.first.url)
-                const song = {
-                    title: songInfo.videoDetails.title,
-                    url: songInfo.videoDetails.video_url
+        if(!vc){
+            return message.channel.send("Please join a voice chat first");
+        }else{
+            let result = await searcher.search(args.join(" "), { type: "video" }) 
+            const songInfo = await ytdl.getInfo(result.first.url)
+
+            let song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            };
+
+            if(!serverQueue){
+                const queueConstructor = {
+                    txtChannel: message.channel,
+                    vChannel: vc,
+                    connection: null,
+                    songs: [],
+                    volume: 10,
+                    playing: true,
+                    loopone: false,
+                    loopall: false,
+                    skipVotes: []
                 };
+                queue.set(message.guild.id, queueConstructor);
 
-                if(!serverQueue){
-                    const queueConstruct = {
-                        txtChannel: message.channel,
-                        vChannel: vc,
-                        connection: null,
-                        songs: [],
-                        volume: 5,
-                        playing: true
-                    };
-                    queue.set(message.guild.id, queueConstruct);
-                        
-                    queueConstruct.songs.push(song);
+                queueConstructor.songs.push(song);
 
-                    try{
-                        let connection = await vc.join();
-                        queueConstruct.connection = connection;
-                        play(message.guild, queueConstruct.songs[0]);
-                    }catch (err){
-                        console.error(`Epic error occured ${err}`);
-                        queue.delete(message.guild.id);
-                        return message.channel.send(err);
-                    }
-                }else{
-                    serverQueue.songs.push(song);
-                    return message.channel.send(`${song.url} has been added to the queue`);
+                try{
+                    let connection = await vc.join();
+                    queueConstructor.connection = connection;
+                    message.guild.me.voice.setSelfDeaf(true);
+                    play(message.guild, queueConstructor.songs[0]);
+                }catch (err){
+                    console.error(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(`Unable to join the voice chat ${err}`)
                 }
             }else{
-                return message.channel.send("You are not in any voice channels to use this command")
+                serverQueue.songs.push(song);
+                let msg = new Discord.MessageEmbed()
+                    .setTitle("Song has been added")
+                    .addField("", song.title)
+
+                return message.channel.send(`The song has been added ${song.url}`);
             }
+        }
     }
-
-    //MUSIC FUNCTIONS
-    function skip (message, serverQueue){
-        if(!message.member.voice.channel)
-            return message.channel.send("You have to be in a voice channel to use this command")
-        if(!serverQueue)
-            return message.channel.send("There is no song to skip");
-        serverQueue.connection.dispatcher.end();    
-    }
-
-    function stop(message, serverQueue) {
-        if(!message.member.voice.channel)
-            return message.channel.send("You have to be in a voice channel to use this command")
-        serverQueue.songs = [];
-        serverQueue.connection.dispatcher.end();
-    }
-
     function play(guild, song){
         const serverQueue = queue.get(guild.id);
         if(!song){
@@ -160,19 +169,143 @@ client.on('message', async(message) =>{
             queue.delete(guild.id);
             return;
         }
-        try{
-            const dispatcher = serverQueue.connection
-                .play(ytdl(song.url))
-                .on("finish", () => {
-                    serverQueue.songs.shift();
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on('finish', () =>{
+                if(serverQueue.loopone){  
                     play(guild, serverQueue.songs[0]);
-                })
-                .on("error", error => console.error(error));
-            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-            serverQueue.txtChannel.send(`Now playing ${song.url}`);
-        }catch(err){
-            console.log(`There was an error ${err}`)
+                }
+                else if(serverQueue.loopall){
+                    serverQueue.songs.push(serverQueue.songs[0])
+                    serverQueue.songs.shift()
+                }else{
+                    serverQueue.songs.shift()
+                }
+                play(guild, serverQueue.songs[0]);
+            })
+
+            let msg = new Discord.MessageEmbed()
+                    .setTitle("Now Playing:")
+                    .addField("------------------------", serverQueue.songs[0].title)
+                    .setColor("BLUE")
+            serverQueue.txtChannel.send(msg)
+            
+    }
+    function stop (message, serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music playing!")
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You need to join the voice chat first!")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+    function skip (message, serverQueue){
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You need to join the voice chat first");
+        if(!serverQueue)
+            return message.channel.send("There is nothing to skip!");
+
+        let roleN = message.guild.roles.cache.find(role => role.name === "DJ")
+
+        if(!message.member.roles.cache.get(roleN.id))
+            return message.channel.send("You don't have the DJ role");
+
+        serverQueue.connection.dispatcher.end();
+        serverQueue.skipVotes = [];
+    }
+
+    function vSkip(serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music currently playing!");
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You are not in the voice channel!");
+        
+        let usersC = message.member.voice.channel.members.size;
+        let required = Math.ceil(usersC/2);
+
+        if(serverQueue.skipVotes.includes(message.member.id))
+            return message.channel.send("You already voted to skip!")
+
+        serverQueue.skipVotes.push(message.member.id)
+        message.channel.send(`You voted to skip the song ${serverQueue.skipVotes.length}/${required} votes`)
+
+        if(serverQueue.skipVotes.length >= required){
+            serverQueue.connection.dispatcher.end();
+            serverQueue.skipVotes = [];
+            message.channel.send("Song has been skipped")
         }
+    }
+
+    function pause(serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music currently playing!");
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You are not in the voice channel!")
+        if(serverQueue.connection.dispatcher.paused)
+            return message.channel.send("The song is already paused");
+        serverQueue.connection.dispatcher.pause();
+        message.channel.send("The song has been paused!");
+    }
+    function resume(serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music currently playing!");
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You are not in the voice channel!")
+        if(serverQueue.connection.dispatcher.resumed)
+            return message.channel.send("The song is already playing!");
+        serverQueue.connection.dispatcher.resume();
+        message.channel.send("The song has been resumed!");
+    }
+    function Loop(args, serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music currently playing!");
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You are not in the voice channel!")
+
+        switch(args[0].toLowerCase()){
+           case 'all':
+               serverQueue.loopall = !serverQueue.loopall;
+               serverQueue.loopone = false;
+
+               if(serverQueue.loopall === true)
+                   message.channel.send("Loop all has been turned on!");
+               else
+                    message.channel.send("Loop all has been truned off!");
+
+               break;
+            case 'one':
+                serverQueue.loopone = !serverQueue.loopone;
+                serverQueue.loopall = false;
+
+                if(serverQueue.loopone === true)
+                    message.channel.send("Loop one has been turned on!");
+                else
+                    message.channel.send("Loop one has been truned off!");
+                break;
+            case 'off':
+                    serverQueue.loopall = false;
+                    serverQueue.loopone = false;
+
+                    message.channel.send("Loop has been turned off!");
+                break;
+            default:
+                message.channel.send("Please specify what loop you want. !loop <one/all/off>"); 
+        }
+    }
+    function Queue(serverQueue){
+        if(!serverQueue)
+            return message.channel.send("There is no music currently playing!");
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You are not in the voice channel!")
+
+        let nowPlaying = serverQueue.songs[0];
+        let qMsg =  `Now playing: ${nowPlaying.title}\n--------------------------\n`
+
+        for(var i = 1; i < serverQueue.songs.length; i++){
+            qMsg += `${i}. ${serverQueue.songs[i].title}\n`
+        }
+
+        message.channel.send('```' + qMsg + 'Requested by: ' + message.author.username + '```');
     }
 
     //NSFW FUNCTIONS
